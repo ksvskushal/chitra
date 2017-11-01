7886a8f66f43d005ba3f76f85d25e9162ee61152
Kushal K S V S <kkushal32@gmail.com>
2017-11-01 22:24:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,9 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in global layout
 app = Flask(__name__)
 
-
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
-
-    res = json.dumps(req, indent=4)
-    res = make_response(res)
-    res.headers['Content-Type'] = 'application/json'
-    return res
-from flask import Response
-
-@app.route('/hello', methods = ['GET'])
+@app.route('/')
 def api_hello():
     data = {
         'hello'  : 'world',
@@ -36,8 +24,4 @@ def api_hello():
     return resp
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-
-    print "Starting app on port %d" % port
-
-app.run(debug=True, port=port, host='0.0.0.0')
+app.run(debug=True,host='0.0.0.0')
