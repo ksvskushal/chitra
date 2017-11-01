9daefc80761093639720aff11ce6428dfef24923
Kushal K S V S <kkushal32@gmail.com>
2017-11-01 20:00:10 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    res = json.dumps(req, indent=4)
+    res = make_response(res)
+    res.headers['Content-Type'] = 'application/json'
+    return res
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+app.run(debug=True, port=port, host='0.0.0.0')
