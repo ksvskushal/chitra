d80d207bbbf285fda239b46fb8ca87bed2417ba7
Kushal K S V S <kkushal32@gmail.com>
2017-11-01 23:38:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def homepage():
     <h1>Hello heroku</h1>
     """
 
+@app.route('/upload/<uuid>', methods=['GET', 'POST'])
+def add_message(uuid):
+    content = request.get_json(silent=True)
+    
+    return """
+    <h1>{cont}</h1>
+    """.format(cont=content)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
