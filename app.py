a2be10e8b92446e409f05b8a5dee486ce0785114
Kushal K S V S <kkushal32@gmail.com>
2017-11-03 21:23:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-from flask import Flask,request
-from datetime import datetime
-import json
+#!/usr/bin/env python
 
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
 
-    return """
-    <h1>Hello heroku</h1>
-    """
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
 
-@app.route('/upload', methods=['GET', 'POST'])
-def add_message():
-    if request.method=='GET':
-      return "This is GET"
-
-    elif request.method == 'POST':  
-      content = request.get_json(silent=True)
-      return json.dumps(content)
+    print("Request:")
+    in_string = json.dumps(req, indent=4)
+    print(in_string)
+    
+    res = {
+        "speech": in_string,
+        "displayText": in_string,
+        #"data": {},
+        # "contextOut": [],
+        "source": "apiai-onlinestore-shipping"
+    }
+    
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+app.run(debug=True, port=port, host='0.0.0.0')
